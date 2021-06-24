e5e7cc12575123887a3faf88b82f3ce3e74e9715
olaaltaslaq <olaaltaslaq@gmail.com>
2021-06-24 18:14:07 +0300

    p2

--- app.js
+++ app.js
@@ -30,12 +30,16 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
-// function multiply(a, b) {
-//   //eslint-disable-line
-// }
+function multiply(a, b) {
+  let num1 = a;
+  let num2 = b;
+  let multiply = num1 * num2;
+  let message = "The product of 5 and 9 is 45."
+  return [multiply, message];
+}
 
 // Here is the test for multiply(); uncomment it to run it
-// testMultiply(5,9);
+testMultiply(5,9);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
